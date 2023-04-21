e5c7e1daf6fc7a301f820b143964df584d2c9cf5
Marek Vykysalý <mara.vyky@gmail.com>
2023-04-21 10:27:47 +0200

    chore: install sass dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hello <span>Svelte!</span></h1>
+
+<style lang="scss">
+    @import '../scss/reset';
+
+    span {
+        color: orangered;
+    }
+</style>
